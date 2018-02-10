d7708e9fd39ff7b24d7af549c6647e0b4dbd796d
Radziievskyi <bog777666@gmail.com>
2018-02-10 18:56:44 +0200

    start lesson 4 enum

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
 public class Main {
 
     public static void main(String[] args) {
-	
+        test(Countries.POLAND);
+
     }
+}
+
+
+static void test(Countries Countries){
+    switch (Countries){
+        case SPAIN:
+            System.out.println("Eurasia");
+            break;
+        case BRAZIL:
+            System.out.println("South america");
+            break;
+        case POLAND:
+            System.out.println("Eurasia");
+            break;
+        case UKRAINE:
+            System.out.println("Eurasia");
+            break;
+        case ARGENTINA:
+            System.out.println("South america");
+            break;
+            default:
+                System.out.println("something wrong");
+    }
+}
+enum Countries{
+    UKRAINE, POLAND,SPAIN, BRAZIL, ARGENTINA
 }
