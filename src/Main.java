0f0d49d164fd77cc98cb026584f6d4ab7573d76f
Radziievskyi <bog777666@gmail.com>
2018-02-10 19:06:44 +0200

    enum ready goin for next step

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,30 @@ public class Main {
         test(Countries.POLAND);
 
     }
-}
 
 
-static void test(Countries Countries){
-    switch (Countries){
-        case SPAIN:
-            System.out.println("Eurasia");
-            break;
-        case BRAZIL:
-            System.out.println("South america");
-            break;
-        case POLAND:
-            System.out.println("Eurasia");
-            break;
-        case UKRAINE:
-            System.out.println("Eurasia");
-            break;
-        case ARGENTINA:
-            System.out.println("South america");
-            break;
+    static void test(Countries Countries) {
+        switch (Countries) {
+            case SPAIN:
+                System.out.println("Eurasia");
+                break;
+            case BRAZIL:
+                System.out.println("South america");
+                break;
+            case POLAND:
+                System.out.println("Eurasia");
+                break;
+            case UKRAINE:
+                System.out.println("Eurasia");
+                break;
+            case ARGENTINA:
+                System.out.println("South america");
+                break;
             default:
                 System.out.println("something wrong");
+        }
     }
 }
-enum Countries{
-    UKRAINE, POLAND,SPAIN, BRAZIL, ARGENTINA
+enum Countries {
+    UKRAINE, POLAND, SPAIN, BRAZIL, ARGENTINA
 }
