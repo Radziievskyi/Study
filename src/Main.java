ad65450291afbfb477f3851184dbc07c6b380e81
Radziievskyi <bog777666@gmail.com>
2018-02-13 19:37:42 +0200

    done 1 2/2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,47 @@
 public class Main {
 
     public static void main(String[] args) {
-        test(Countries.POLAND);
+        test(Countries.BRAZIL);
+        int[] mass = {5, -6, 8, 17, -36, 48, 22, 7, -18, -3};
+        for (int i = 0; i < mass.length; i++) {
+            System.out.print(mass[i] + ", ");
+        }
+
+        int max = mass[0];
+        int z = 0;
+
+        while (z < mass.length) {
+            if (mass[z] > max) {
+                max = mass[z];
+            }
+            z++;
+        }
+        System.out.println("\n" + "max=" + max);
+
+
+        int sum = 0;
+        for (int b = 0; b < mass.length; b++) {
+            if (mass[b] > 0)
+                sum = sum + mass[b];
+        }
+        System.out.println("sumPositive=" + sum);
+
+        int sumAll = 0;
+        for (int p = 0; p < mass.length; p++) {
+            sumAll += mass[p];
+        }
+        System.out.println("sumAll=" + sumAll);
+
+
 
     }
+
+
+
+
+
+
+
 
 
     static void test(Countries Countries) {
